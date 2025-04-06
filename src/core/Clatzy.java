c3fc3f5c06dc260fe1402876288d57a44117567f
Jeronimo <jecastroam@gmail.com>
2025-04-05 23:16:48 -0500

    Aniadimos clases Clatzy, Persona, Cliente e Instructor

--- src/core/Clatzy.java
+++ src/core/Clatzy.java
@@ -1,5 +1,17 @@
 package core;
 
+import java.util.ArrayList;
+
 public class Clatzy {
-    
+    private ArrayList<Cliente> clientes;
+    private ArrayList<Instructor> instructores;
+    private ArrayList<Curso> cursos;
+    private ArrayList<Plan> planes;
+
+    public Clatzy() {
+        this.clientes = new ArrayList<>();
+        this.instructores = new ArrayList<>();
+        this.cursos = new ArrayList<>();
+        this.planes = new ArrayList<>();
+    }
 }
---
+++ src/core/Cliente.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package core;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author jecas
+ */
+public class Cliente extends Persona{
+    private ArrayList<PlanCliente> planes;
+    private ArrayList<ProductoCliente> productos;
+
+    public Cliente(String nombre, String cedula, String telefono, String email) {
+        super(nombre, cedula, telefono, email);
+        this.planes = new ArrayList<>();
+        this.productos = new ArrayList<>();
+    }
+}
